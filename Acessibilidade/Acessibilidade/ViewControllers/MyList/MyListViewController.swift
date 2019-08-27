34955bb9bedaff274db59062a5b6907f1d716c08
Juliana Vigato Pavan <juvigato@10-44-48-199.0200.mack.br.iosda.org>
2019-08-27 16:14:48 -0300

    git ignore adicionado

---
+++ Acessibilidade/Acessibilidade/ViewControllers/MyList/MyListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MyListViewController.swift
+//  Acessibilidade
+//
+//  Created by Juliana Vigato Pavan on 27/08/19.
+//  Copyright © 2019 Felipe Petersen. All rights reserved.
+//
+
+import UIKit
+
+class MyListViewController: UIViewController {
+    
+    @IBOutlet weak var testeLabel: UILabel!
+    @IBOutlet weak var testeTableView: UITableView!
+    
+    let viewModel = MyListViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTableView()
+    }
+    
+    func setTableView(){
+        self.testeTableView.delegate = self
+        self.testeTableView.dataSource = self
+    }
+
+}
+
+extension MyListViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
